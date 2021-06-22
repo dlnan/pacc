930df0f74658316502b4065dec4a0716764309c8
C1 <zj175@139.com>
2021-06-22 19:34:00 +0800

    更新于2021年06月22日	19:33:59.99

--- setup - 副本.bat
+++ setup - 副本.bat
@@ -4,6 +4,7 @@ twine upload -u coco56 -p password dist/*
 git add .
 git commit -m "更新于%date:~0,4%年%date:~5,2%月%date:~8,2%日	%time%"
 git push -f origin main
-timeout /t 60
-pip3 install --upgrade pacc
+pip install --upgrade pacc
+timeout /t 15
+pip install --upgrade pacc
 pause
