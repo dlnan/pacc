5b8ed5bb3ebab30d7233784281554ac42016ed5f
C1 <zj175@139.com>
2021-06-24 07:12:43 +0800

    更新于2021年06月24日	 7:12:42.80

--- pacc/mysql/mysql.py
+++ pacc/mysql/mysql.py
@@ -1,5 +1,6 @@
-from pymysql import connect
+from pymysql import connect, OperationalError
 import os
+from ..tools import sleep
 
 
 class Config:
@@ -17,8 +18,14 @@ Config()
 
 
 def query(cmd):
-    Config.cs.execute(cmd)
-    res = Config.cs.fetchall()
+    try:
+        Config.cs.execute(cmd)
+        res = Config.cs.fetchall()
+    except OperationalError as e:
+        print('query', e)
+        sleep(30)
+        Config()
+        return query(cmd)
     if len(res) == 1:
         res = res[0]
     return res
@@ -26,7 +33,13 @@ def query(cmd):
 
 def commit():
     # 提交之前的操作，如果之前已经之执行过多次的execute，那么就都进行提交
-    Config.conn.commit()
+    try:
+        Config.conn.commit()
+    except OperationalError as e:
+        print('commit', e)
+        sleep(30)
+        Config()
+        commit()
 
 
 class Retrieve:
--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pacc',  # 包名
-    version='0.0.12',  # 版本
+    version='0.0.13',  # 版本
     packages=find_packages(),  # 目录下所有文件
     description='Python Android Cluster Control 为使用Python对安卓集群控制进行支持',  # 描述信息
     long_description="""该Python模块用于通过USB或Wifi对Android集群进行控制，集成了ADB、数据库、图像识别、中央控制系统、验证码破解、等功能。
-    目前最新版的是0.0.12，主要实现了以下功能：
+    目前最新版的是0.0.13，已集成的功能如下：
+    1. ADB（安卓调试桥）
+    2. MySQL数据库
+    已实现的中央控制系统如下：
     1. 抖音全自动抢福袋脚本app中央控制系统
     2. 汉字大英雄全自动看广告脚本app中央控制系统
     3. 淘宝/拼多多全自动远程刷单app中央控制系统
