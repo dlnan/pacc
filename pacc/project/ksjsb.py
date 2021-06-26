28712adaaa3a87d01320b93c0185b2bfebe2b338
C1 <zj175@139.com>
2021-06-27 07:47:55 +0800

    更新于2021年06月27日	 7:47:54.90

--- pacc/project/ksjsb.py
+++ pacc/project/ksjsb.py
@@ -10,37 +10,50 @@ class KSJSB(Project):
     verificationCode = 'com.kuaishou.nebula/com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity'
     liveStreaming = 'com.kuaishou.nebula/com.yxcorp.gifshow.detail.PhotoDetailActivity'
     userProfileActivity = 'com.kuaishou.nebula/com.yxcorp.gifshow.profile.activity.UserProfileActivity'
+    instances = []
+    startTime = datetime.now()
 
     def __init__(self, deviceSN):
-        self.startTime = datetime.now()
         super(KSJSB, self).__init__(deviceSN)
+        self.sleepTime = 0
 
     def tapFreeButton(self):
         super(KSJSB, self).tapFreeButton(540, 1706)
 
     def randomSwipe(self):
-        r = randint(6, 30)
+        if self.sleepTime > 0:
+            return
         x1 = randint(500, 560)
         y1 = randint(1500, 1590)
         x2 = randint(500, 560)
         y2 = randint(360, 560)
         self.adbIns.swipe(x1, y1, x2, y2)
-        sleep(r)
+        self.sleepTime += randint(6, 30)
 
     def openApp(self):
         super(KSJSB, self).openApp('com.kuaishou.nebula/com.yxcorp.gifshow.HomeActivity')
 
-    def mainloop(self):
+    def start(self):
         self.adbIns.reboot()
         self.freeMemory()
         self.openApp()
+
+    @classmethod
+    def mainloop(cls):
+        cls.instances.append(cls('302'))
+        cls.instances.append(cls('303'))
+        for i in cls.instances:
+            i.start()
         while True:
-            self.randomSwipe()
-            print('已运行：', datetime.now() - self.startTime, sep='')
-            if (self.liveStreaming or self.userProfileActivity)\
-                    in self.adbIns.getCurrentFocus():
-                self.adbIns.reboot()
-                self.freeMemory()
-                self.openApp()
-            elif self.verificationCode in self.adbIns.getCurrentFocus():
-                EMail(self.adbIns.device.SN).sendVerificationCodeAlarm()
+            st = randint(3, 9)
+            for i in cls.instances:
+                i.randomSwipe()
+                i.sleepTime -= st
+            print('已运行：', datetime.now() - cls.startTime, sep='')
+            for i in cls.instances:
+                if (i.liveStreaming or i.userProfileActivity)\
+                        in i.adbIns.getCurrentFocus():
+                    i.start()
+                elif i.verificationCode in i.adbIns.getCurrentFocus():
+                    EMail(i.adbIns.device.SN).sendVerificationCodeAlarm()
+            sleep(st)
