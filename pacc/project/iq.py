165012aebee1cfcff3f0ecfbeb0ae7daf5a767b9
C1 <zj175@139.com>
2021-08-01 08:01:44 +0800

    更新于2021年08月01日	 8:01:44.21

--- pacc/project/iq.py
+++ pacc/project/iq.py
@@ -2,18 +2,16 @@ from .project import Project
 from ..tools import sleep
 
 
+class ResourceID:
+    icon_title = 'com.miui.home:id/icon_title'  # 桌面图标
+
+
 class IQ(Project):
-    """
-    """
-    scriptName = 'com.kildare.nautor/com.stardust.autojs.execution.ScriptExecuteActivity'
-
-    instances = []
-
     def __init__(self):
         super(IQ, self).__init__('201')
 
     def mainloop(self):
         while True:
             if self.adbIns.rebootPerHour():
-                self.uIAIns.tap(130, 235)
+                self.uIAIns.click(ResourceID.icon_title, '新自阅')
             sleep(1200)
