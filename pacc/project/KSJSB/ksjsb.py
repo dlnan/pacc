c40517b4b668a9886f392fe1351b069bf04d7fcd
C1 <zj175@139.com>
2021-08-03 10:22:16 +0800

    更新于2021年08月03日	10:22:16.57

--- pacc/project/KSJSB/ksjsb.py
+++ pacc/project/KSJSB/ksjsb.py
@@ -2,6 +2,7 @@ import xml
 from random import randint
 from datetime import datetime, timedelta
 from time import time
+import time
 from ...tools import sleep
 from ...mysql import RetrieveKSJSB, UpdateKSJSB
 from ...Multi import runThreadsWithArgsList, runThreadsWithFunctions, threadLock
@@ -131,7 +132,8 @@ class KSJSB(Project):
         self.reopenAppPerHour()
         try:
             if not datetime.now().day == self.startDay:
-                sleep(60)
+                print(self.adbIns.device.SN, '不在工作期', sep='')
+                time.sleep(60)
                 return
             if datetime.now().hour > 8 and self.uIAIns.getDict(resourceID.red_packet_anim):
                 if not self.uIAIns.getDict(resourceID.cycle_progress, xml=self.uIAIns.xml):
