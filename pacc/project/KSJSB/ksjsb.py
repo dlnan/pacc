62cffcadb5fe9e92e1478a53a4479e0cfefd94df
C1 <zj175@139.com>
2021-08-03 10:33:28 +0800

    更新于2021年08月03日	10:33:28.27

--- pacc/project/KSJSB/ksjsb.py
+++ pacc/project/KSJSB/ksjsb.py
@@ -132,7 +132,7 @@ class KSJSB(Project):
         try:
             if not datetime.now().day == self.startDay:
                 print(self.adbIns.device.SN, '不在工作期', sep='')
-                sleep(60, False)
+                sleep(6)
                 return
             if datetime.now().hour > 8 and self.uIAIns.getDict(resourceID.red_packet_anim):
                 if not self.uIAIns.getDict(resourceID.cycle_progress, xml=self.uIAIns.xml):
